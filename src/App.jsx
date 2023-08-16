c700126159aafc2b33faa6b6bb925fe417d1b5c4
AilenGonzalez <ailu.gonzalez@hotmail.com>
2023-08-16 20:18:16 -0300

    tercer commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,3 @@
-/*import Card from "./Card";
-import './App.css';
-
-
-function App() {
-  return (
-    <div className="App">
-      <h1>Carga de estudiantes</h1>
-      <form>
-
-      
-      </form>
-      <Card />
-    </div>
-  );
-}
-
-
-export default App;*/
-
 import React from 'react';
 import './App.css';
 import UñasForm from './UñasForm';
